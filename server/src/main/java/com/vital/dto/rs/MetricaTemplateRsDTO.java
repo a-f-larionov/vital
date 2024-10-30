884bf776a2e7295d3c4b96b968417f74d57067c5
a.f.larionov <a.f.larionov@gmail.com>
2024-10-30 22:18:20 +0300

    update

---
+++ server/src/main/java/com/vital/dto/rs/MetricaTemplateRsDTO.java
@@ -0,0 +1,36 @@
+package com.vital.dto.rs;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MetricaTemplateRsDTO {
+
+
+    private String id;
+
+    @NotNull
+    private Long sort;
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String shortTitle;
+
+    @NotBlank
+    private String icon;
+
+    @NotBlank
+    private String typeCode;
+
+    @NotBlank
+    private String inputCode;
+
+    @NotBlank
+    private String viewCode;
+
+}
